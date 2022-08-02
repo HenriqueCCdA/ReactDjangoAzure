5324d795c3febb529b10f5c12c4ec9a7656930d1
Henrique Andrade <henrique.ccda@gmail.com>
2022-08-02 14:46:53 -0300

    Aula 37

--- frontend/src/pages/RegistrationPage/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -76,87 +76,91 @@ function RegistrationPage() {
         setSuccess(false);
     };
 
-    if (results) {
-        if (success) {
-            return (
-                <div>
-                    <h1> Verify Email </h1>
-                    <p> A verification email has been sent to {email}. </p>
-                    <p> Please click on the link in the email to verify your account. </p>
-                </div>
-            )
-        } else {
-            return <div>Unable to complete registration</div>;
-        }
+    if (loading) {
+        return (<div>Loading</div>);
     } else {
 
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>Register</h1>
-                        <Form onSubmit={submitHandler}>
-                            <Form.Group controlId='name'>
-                                <Form.Label>Full Name</Form.Label>
-                                <Form.Control
-                                    type='name'
-                                    placeholder='Enter Full Name'
-                                    value={name}
-                                    onChange={e => setName(e.target.value)}
-                                />
-                            </Form.Group>
-                            <Form.Group controlId='email'>
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control
-                                    type='email'
-                                    placeholder='Enter Email'
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)} />
-                            </Form.Group>
-                            <Form.Group controlId="password">
-                                <Form.Label>Enter Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Enter password"
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)} />
-                            </Form.Group>
-                            <Form.Group controlId='confirmPassword'>
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control
-                                    type='password'
-                                    placeholder='Confirm password'
-                                    value={confirmPassword}
-                                    onChange={e => setConfirmPassword(e.target.value)} />
-                            </Form.Group>
-                            {passwordTyped ?
-                                passwordGood ?
-                                    (
-                                        <div>Password complexity is good</div>
-                                    ) :
-                                    (
-                                        <Alert variant='danger' sytle={{ backgroundColor: 'yellow' }}>
-                                            {checkPasswordComplexity(password, passwordGood).map(e => {
-                                                if (e) {
-                                                    return <li key={e}>{e}</li>
-                                                }
-                                            })}
-                                        </Alert>
-                                    ) : (
-                                    <div></div>
-                                )
-                            }
-                            <Button type='submit' variant='primary' disabled={!submitButtonEnabled}>Register</Button>
-                        </Form>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Link to='login'>Sign In</Link> if you have an account already!
-                    </Col>
-                </Row>
-            </Container>
-        )
+        if (results) {
+            if (success) {
+                return (
+                    <div>
+                        <h1> Verify Email </h1>
+                        <p> A verification email has been sent to {email}. </p>
+                        <p> Please click on the link in the email to verify your account. </p>
+                    </div>
+                )
+            } else {
+                return <div>Unable to complete registration</div>;
+            }
+        } else {
+            return (
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>Register</h1>
+                            <Form onSubmit={submitHandler}>
+                                <Form.Group controlId='name'>
+                                    <Form.Label>Full Name</Form.Label>
+                                    <Form.Control
+                                        type='name'
+                                        placeholder='Enter Full Name'
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId='email'>
+                                    <Form.Label>Email Address</Form.Label>
+                                    <Form.Control
+                                        type='email'
+                                        placeholder='Enter Email'
+                                        value={email}
+                                        onChange={e => setEmail(e.target.value)} />
+                                </Form.Group>
+                                <Form.Group controlId="password">
+                                    <Form.Label>Enter Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Enter password"
+                                        value={password}
+                                        onChange={e => setPassword(e.target.value)} />
+                                </Form.Group>
+                                <Form.Group controlId='confirmPassword'>
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control
+                                        type='password'
+                                        placeholder='Confirm password'
+                                        value={confirmPassword}
+                                        onChange={e => setConfirmPassword(e.target.value)} />
+                                </Form.Group>
+                                {passwordTyped ?
+                                    passwordGood ?
+                                        (
+                                            <div>Password complexity is good</div>
+                                        ) :
+                                        (
+                                            <Alert variant='danger' sytle={{ backgroundColor: 'yellow' }}>
+                                                {checkPasswordComplexity(password, passwordGood).map(e => {
+                                                    if (e) {
+                                                        return <li key={e}>{e}</li>
+                                                    }
+                                                })}
+                                            </Alert>
+                                        ) : (
+                                        <div></div>
+                                    )
+                                }
+                                <Button type='submit' variant='primary' disabled={!submitButtonEnabled}>Register</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Link to='login'>Sign In</Link> if you have an account already!
+                        </Col>
+                    </Row>
+                </Container>
+            )
+        }
     }
 }
 
