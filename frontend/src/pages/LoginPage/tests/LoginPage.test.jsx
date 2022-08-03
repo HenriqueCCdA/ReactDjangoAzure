b1ded16dde35412b195812ee690659bc309331fb
Henrique Andrade <henrique.ccda@gmail.com>
2022-08-03 15:00:59 -0300

    Aula 42

--- frontend/src/pages/LoginPage/tests/LoginPage.test.jsx
+++ frontend/src/pages/LoginPage/tests/LoginPage.test.jsx
@@ -38,3 +38,38 @@ test("User can log in", async () => {
     const homepageText = await screen.findByText(/Welcome to the HomePage/i);
     expect(homepageText).toBeInTheDocument();
 });
+
+test("User cannot log in", async () => {
+    render(<App />);
+
+    const loginLink = await screen.findByRole("link", { name: /login/i });
+    expect(loginLink).toBeInTheDocument();
+    userEvent.click(loginLink);
+
+    var emailField = await screen.findByRole("textbox", {
+        name: /email address/i,
+    });
+    expect(emailField).toBeInTheDocument();
+    var passwordField = await screen.findByLabelText(/password/i);
+    expect(passwordField).toBeInTheDocument();
+    var loginButton = await screen.findByRole("button", { name: /log in/i });
+    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toBeDisabled();
+
+    userEvent.clear(emailField);
+    userEvent.type(emailField, "james@example.com");
+    userEvent.clear(passwordField);
+    userEvent.type(passwordField, "WrongPassword!");
+    userEvent.click(loginButton);
+    expect(loginButton).toBeEnabled();
+
+    const errorSigningIn = await screen.findByText(/Error Logging In/i);
+    expect(errorSigningIn).toBeInTheDocument();
+
+    emailField = await screen.findByRole("textbox", { name: /email address/i });
+    expect(emailField).toBeInTheDocument();
+    passwordField = await screen.findByLabelText(/password/i);
+    expect(passwordField).toBeInTheDocument();
+    loginButton = await screen.findByRole("button", { name: /log in/i });
+    expect(loginButton).toBeInTheDocument();
+});
